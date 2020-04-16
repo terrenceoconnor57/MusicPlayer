8c6c9c62d9e455cad84e306b4b74cdf4f923a1da
terrenceoconnor57 <60070754+terrenceoconnor57@users.noreply.github.com>
2020-04-16 17:13:48 -0400

    Need to add styling and more songs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-
+import './App.css';
+import MusicPlayer from 'react-responsive-music-player';
 
 class App extends Component {
   render() {
-    return (
-      <div>
 
+    const playlist = [
+  {
+    url: 'lead groove with piano.mp3',
+    cover: 'mee.png',
+    title: 'Someday',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+  {
+    url: 'path/to/mp3',
+    cover: 'path/to/jpg',
+    title: 'Bedtime Stories',
+    artist: [
+      'Jay Chou'
+    ]
+  }
+]
+
+
+
+    return (
+      <div className = 'center'>
+      <div className = 'cent'>
+        <MusicPlayer playlist={playlist} />
+        </div>
       </div>
     );
   }
